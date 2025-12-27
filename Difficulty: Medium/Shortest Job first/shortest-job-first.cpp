26a081732042b8ee38fbf0c820152af1f7fdac3f
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-12-27 20:28:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
+++ Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
@@ -5,19 +5,14 @@
 class Solution {
   public:
     long long solve(vector<int>& bt) {
+        long long t=0;
+        long long wt=0;
         long long n=bt.size();
-        vector<long long>ct(n);
         sort(bt.begin(),bt.end());
-        ct[0]=bt[0];
-        for(long long i=1;i<n;i++){
-            ct[i]=ct[i-1]+bt[i];
-        }
-        vector<long long>wt(n);
         for(long long i=0;i<n;i++){
-            wt[i]=ct[i]-bt[i];
+            wt+=t;
+            t+=bt[i];
         }
-        long long sum=accumulate(wt.begin(),wt.end(),0);
-        long long waitingt=sum/n;
-        return waitingt;
+        return wt/n;
     }
 };
