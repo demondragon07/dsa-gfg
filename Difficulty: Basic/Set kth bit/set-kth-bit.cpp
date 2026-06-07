22dfc901877249a0e8389fe1786aaf8408f22028
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-06-07 13:07:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Set kth bit/set-kth-bit.cpp
+++ Difficulty: Basic/Set kth bit/set-kth-bit.cpp
@@ -1,11 +1,7 @@
 class Solution {
   public:
     int setKthBit(int n, int k) {
-         int i=1;
-         int mask=1<<k;
-         int ans=n;
-         ans=ans|mask;
-         
-        return ans;
+       return n | (1<<k);
+        
     }
 };
