a763efc011949bc9db8fa223165d9628bf511ec1
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-06-10 18:51:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/All Prime Factors in Any Order/all-prime-factors-in-any-order.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> primeFactors(int n) {
+      vector<int>ans;
+       for(int i=2;i*i<=n;i++){
+           if(n%i==0){
+               while(n%i==0){
+                ans.push_back(i);
+                   n=n/i;
+               }
+           }  
+       }
+      if(n!=1)ans.push_back(n);
+       return ans;
+    }
+};
