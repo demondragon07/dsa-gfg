264305d89cbce4c725ce434789373f00a260a430
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-09-09 16:49:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,63 +1,33 @@
 class Solution {
   public:
   
-    // bool DFS(int u,unordered_map<int,vector<int>>&adj,vector<bool>&vis,vector<bool>&inrec){
-    //     vis[u]=true;
-    //     inrec[u]=true;
+    bool DFS(int node , vector<int>adj[],vector<int>&vis ,vector<int>&path){
+        vis[node]=1;
+        path[node]=1;
+        for(int i=0;i<adj[node].size();i++){
+            if(!vis[adj[node][i]]){
+                if(DFS(adj[node][i],adj,vis,path)) return true;
+            }
+           else if(path[adj[node][i]]) return true;
+        }
+        path[node]=0;
+        return false;
+    }
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        vector<int>vis(V,0);
+        vector<int>path(V,0);
         
-    //     for(auto v:adj[u]){
-    //         if(vis[v]==false && DFS(v,adj,vis,inrec)) return true;
-    //         else if(inrec[v]==true) return true;
-    //     }
-        
-    //     inrec[u]=false;
-    //     return false;
-    // }
-    
-    
-    bool isCyclic(int V, vector<vector<int>>&edges) {
-        // vector<bool>vis(V,false);
-        // vector<bool>inrec(V,false);
-        //  unordered_map<int,vector<int>>adj;
-        //  for(auto x:edges){
-        //      int u=x[0];
-        //      int v=x[1];
-        //      adj[u].push_back(v);
-        //  }
-        // for(int i=0;i<V;i++){
-        //     if(!vis[i] && DFS(i,adj,vis,inrec)) 
-        //       return true;
-        // }
-        // return false;
-        
-        //BFS---
-        
-        unordered_map<int,vector<int>>adj;
-        vector<int>indegree(V,0);
+        vector<int>adj[V];
         for(auto x:edges){
             int u=x[0];
             int v=x[1];
             adj[u].push_back(v);
-            indegree[v]++;
         }
         
-       queue<int>q;
-       for(int i=0;i<V;i++){
-          if(indegree[i]==0) q.push(i);
-       }
-       vector<int>result;
-       while(!q.empty()){
-           int u=q.front();
-           result.push_back(u);
-           q.pop();
-           for(auto v:adj[u]){
-               indegree[v]--;
-               if(indegree[v]==0) q.push(v);
-           }
-       }
+        for(int i=0;i<V;i++){
+            if(DFS(i,adj,vis,path)) return true;
+        }
         
-        if(result.size()==V) return false;
-        else return true;
-        
+        return 0;
     }
 };
