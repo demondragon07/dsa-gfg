ad685369f8d1ce03fdbdc960413fef8d01435e5b
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-09-13 02:25:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,33 +1,33 @@
 class Solution {
   public:
-  
-    bool DFS(int node , vector<int>adj[],vector<int>&vis ,vector<int>&path){
-        vis[node]=1;
-        path[node]=1;
-        for(int i=0;i<adj[node].size();i++){
-            if(!vis[adj[node][i]]){
-                if(DFS(adj[node][i],adj,vis,path)) return true;
-            }
-           else if(path[adj[node][i]]) return true;
-        }
-        path[node]=0;
-        return false;
-    }
     bool isCyclic(int V, vector<vector<int>> &edges) {
-        vector<int>vis(V,0);
-        vector<int>path(V,0);
-        
+        vector<int>indeg(V,0);
         vector<int>adj[V];
         for(auto x:edges){
             int u=x[0];
             int v=x[1];
             adj[u].push_back(v);
+            indeg[v]++;
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indeg[i]==0)q.push(i);
+        }
+        
+        while(!q.empty()){
+            auto node=q.front();
+            q.pop();
+            for(int i=0;i<adj[node].size();i++){
+                indeg[adj[node][i]]--;
+                if(indeg[adj[node][i]]==0)q.push(adj[node][i]);
+            }
         }
         
         for(int i=0;i<V;i++){
-            if(DFS(i,adj,vis,path)) return true;
+            if(indeg[i]!=0)return true;  
         }
+        return false;
         
-        return 0;
+        
     }
 };
